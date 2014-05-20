0771b15a5ed576a18d06249ad4caf4bde3c3d7a3
saraibisseling <saraibisseling@yahoo.co.uk>
2014-05-20 12:53:46 +0200

    all 17 tests passed of part 5. using nicks eval.'

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -1,141 +1,106 @@
 # -*- coding: utf-8 -*-
 
-
 from types import Environment, LispError, Closure
 from ast import is_boolean, is_atom, is_symbol, is_list, is_closure, is_integer
 from asserts import assert_exp_length, assert_valid_definition, assert_boolean
-from parser import parse
-
+from parser import unparse
 
 """
 This is the Evaluator module. The `evaluate` function below is the heart
 of your language, and the focus for most of parts 2 through 6.
 
-A score of useful functions is provided for you, as per the above imports,
-making your work a bit easier. (We're supposed to get through this thing
+A score of useful functions is provided for you, as per the above imports, 
+making your work a bit easier. (We're supposed to get through this thing 
 in a day, after all.)
 """
 
-# define operators
-math_operators = ["+", "-", "/", ">", "<", "mod", "*"]
-
-
 def evaluate(ast, env):
-    """Evaluate an Abstract Syntax Tree in the specified environment."""
+	"""Evaluate an Abstract Syntax Tree in the specified environment."""
+	if is_boolean(ast) or is_integer(ast): # evaluate booleans and integers
+		return ast
+	elif is_symbol(ast): # evaluate symbols
+		return env.lookup(ast)
+	elif is_list(ast):
+		if is_closure(ast[0]):
+			return eval_closure(ast, env)
+		elif ast[0] == 'quote': # evaluate quotes
+			return ast[1]
+		elif ast[0] == 'atom': # evaluate atoms
+			return is_atom(evaluate(ast[1], env))
+		elif ast[0] == 'eq': # evaluate equality
+			a1 = evaluate(ast[1], env)
+			a2 = evaluate(ast[2], env)
+			return is_atom(a1) and is_atom(a2) and a1 == a2
+		# evaluate basic math operators:
+		elif ast[0] in ['+', '-', '/', '*', 'mod', '>', '<', '=']:
+			return eval_math(ast, env)
+		elif ast[0] == 'if': # evaluate if expression
+			return eval_if(ast, env)
+		elif ast[0] == 'define': # evaluate define statement
+			eval_define(ast, env)
+		elif ast[0] == 'lambda':
+			return eval_lambda(ast, env)
+		elif is_symbol(ast[0]) or is_list(ast[0]):
+			closure = evaluate(ast[0], env)
+			return evaluate([closure] + ast[1:], env)
+		else:
+			raise LispError('Argument is not a function!')
 
+def eval_math(ast, env):
+	"""Evaluate an mathematical operator and its
+	arguments in the specified environment.
+	Mathematical operations are carried out by the corresponding
+	mathematical operators built into python."""
+	a1 = evaluate(ast[1], env)
+	a2 = evaluate(ast[2], env)
+	if is_integer(a1) and is_integer(a2):
+		operators = {
+		'+': a1 + a2,
+		'-': a1 - a2,
+		'/': a1 / a2,
+		'*': a1 * a2,
+		'mod': a1 % a2,
+		'>': a1 > a2,
+		'<': a1 < a2,
+		'=': a1 == a2}
+		return operators[ast[0]]
+	else:
+		raise LispError("Math operators only work on integers!")
 
-    # ast = boolean
-    if is_boolean(ast):
-        return ast
+def eval_if(ast, env):
+	"""Evaluate an if expression in the specified environment."""
+	if evaluate(ast[1], env):
+		return evaluate(ast[2], env)
+	else:
+		return evaluate(ast[3], env)
 
-    # ast = integer
-    if is_integer(ast):
-        return ast
+def eval_define(ast, env):
+	"""Evaluate a define statement in the specified environment."""
+	if len(ast) != 3:
+		raise LispError('wrong number of arguments: %d' % (len(ast) - 2))
+	elif not is_symbol(ast[1]):
+		raise LispError('Illegal use of define with non-symbol as variable')
+	else:
+		env.set(ast[1], evaluate(ast[2], env))
 
-    # ast starts with "quote"
-    if ast[0] == "quote":
-        return ast[1]
+def eval_lambda(ast, env):
+	if len(ast) != 3:
+		raise LispError('wrong number of arguments: %d' % (len(ast) - 2))
+	elif not is_list(ast[1]):
+		raise LispError('The parameters to lambda need to be a list!')
+	else:
+		return Closure(env, ast[1], ast[2])
 
-    if ast[0] == "atom":
-        return is_atom(evaluate(ast[1], env))
-
-    if ast[0] == "eq":
-        a = evaluate(ast[1], env)
-        b = evaluate(ast[2], env)
-        if is_atom(a) == False or is_atom(b) == False:
-            return False
-
-        return a == b
-
-    if ast[0] in math_operators:
-        a = evaluate(ast[1], env)
-        b = evaluate(ast[2], env)
-        if is_integer(a) and is_integer(b):
-
-            if ast[0] == "+":
-                return a + b
-            if ast[0] == "*":
-                return a * b
-            if ast[0] == "-":
-                return a - b
-            if ast[0] == "/":
-                return a / b
-            if ast[0] == "mod":
-                return a % b
-            if ast[0] == ">":
-                return a > b
-            if ast[0] == "<":
-                return a < b
-
-    if ast[0] == "if":
-        if evaluate(ast[1], env) == True:
-            return evaluate(ast[2], env)
-        else:
-            return evaluate(ast[3], env)
-
-    if is_symbol(ast):
-        return env.lookup(ast)
-
-    if ast[0] == "define":
-        if not len(ast) == 3:
-            raise LispError("Wrong number of arguments")
-        if not is_symbol(ast[1]):
-            raise LispError("non-symbol")
-        s = ast[1]
-        v = evaluate(ast[2], env)
-        env.set(s, v)
-        print s
-        return s
-
-    if ast[0] == "lambda":
-        print ast[0]
-        print ast[0]
-        if not len(ast) == 3:
-            raise LispError("number of arguments")
-        if not is_list(ast[1]):
-            raise LispError("params not a list")
-        return Closure(env, ast[1], ast[2])
-
-    if is_closure(ast[0]):
-        closure = ast[0]
-        arguments = ast[1:]
-        parameters = closure.params
-        m = len(parameters)
-        n = len(arguments)
-        if not n == m:
-            raise LispError("wrong number of arguments, expected %d got %d" % ((m), n))
-        for i in range(n):
-            closure.env.set(parameters[i], evaluate(arguments[i], env))
-        return evaluate(closure.body, closure.env)
-
-    if is_symbol(ast[0]):
-        clos = env.lookup(ast[0])
-        print clos
-        arguments = ast[1:]
-        parameters = clos.params
-        m = len(parameters)
-        n = len(arguments)
-        if not n == m:
-            raise LispError("wrong number of arguments, expected %d got %d" % ((m), n))
-        for i in range(n):
-            clos.env.set(parameters[i], evaluate(arguments[i], env))
-        return evaluate(clos.body, clos.env)
-
-    if is_list(ast[0]):
-        clos = evaluate(ast[0], env)
-        print clos
-        arguments = ast[1:]
-        parameters = clos.params
-        m = len(parameters)
-        n = len(arguments)
-        if not n == m:
-            raise LispError("wrong number of arguments, expected %d got %d" % ((m), n))
-
-        for i in range(n):
-            clos.env.set(parameters[i], evaluate(arguments[i], env))
-        return evaluate(clos.body, clos.env)
-    else:
-        raise LispError("not a function")
-
-    print ast
-    raise LispError("Not able to evaluate")
+def eval_closure(ast, env):
+	closure = ast[0]
+	arguments = ast[1:]
+	parameters = closure.params
+	if len(arguments) != len(parameters):
+		raise LispError('wrong number of arguments, expected %d got %d'
+			% (len(parameters), len(arguments)))
+	else:
+		for i in range(len(arguments)):
+			arg = evaluate(arguments[i], env)
+			param = parameters[i]
+			closure.env = closure.env.extend({param: arg})
+		return evaluate(closure.body, closure.env)
